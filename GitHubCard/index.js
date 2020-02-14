846c1fcc41bca65a04beab3bad734e3264940b04
Salih Babekr <salihmorta17@gmail.com>
2020-02-14 06:31:01 -0500

    mvp completed

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -2,6 +2,18 @@
            (replacing the palceholder with your Github name):
            https://api.github.com/users/<your name>
 */
+axios.get('https://api.github.com/users/salih-b')
+    .then( response => {
+        // Remember response is an object, response.data is an array.
+        console.log(response);
+        let UserInfo = UserCard(response);
+        let cards = document.querySelector('.cards');
+        cards.append(UserInfo);
+    })
+    .catch( error => {
+        console.log("Error:", error);
+    });
+
 
 /* Step 2: Inspect and study the data coming back, this is YOUR 
    github info! You will need to understand the structure of this 
@@ -24,7 +36,12 @@
           user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const followersArray = [
+  'tetondan',
+  'dustinmyers',
+  'justsml',
+  'luishrd',
+  'bigknell'];
 
 /* Step 3: Create a function that accepts a single object as its only argument,
           Using DOM methods and properties, create a component that will return the following DOM element:
@@ -45,6 +62,55 @@ const followersArray = [];
 </div>
 
 */
+function UserCard(userObj){
+  let mainCard = document.createElement('div');
+  let userImg = document.createElement('img');
+  let cardInfo = document.createElement('div');
+  let titleName = document.createElement('h3');
+  let username = document.createElement('p');
+  let location = document.createElement('p');
+  let profile = document.createElement('p');
+  let aTagAddress = document.createElement('a');
+  let followers = document.createElement('p');
+  let following = document.createElement('p');
+  let bio = document.createElement('p');
+
+  // img and text assignment
+  userImg.src = userObj.data.avatar_url;
+  titleName.textContent = userObj.data.name;
+  username.textContent = userObj.data.login;
+  location.textContent = `Location: ${userObj.data.location}`;
+  aTagAddress.textContent = userObj.data.html_url;
+  profile.textContent = "Profile: ";
+  followers.textContent = `Followers: ${userObj.data.followers}`;
+  following.textContent = `Following: ${userObj.data.following}`;
+  bio.textContent = `Bio: ${userObj.data.bio}`;
+  aTagAddress.setAttribute("href", userObj.data.html_url);
+
+
+  // Classes
+  mainCard.classList.add('card');
+  cardInfo.classList.add('card-info');
+  titleName.classList.add('name');
+  username.classList.add('username');
+
+  // appendments
+  mainCard.append(userImg);
+  mainCard.append(cardInfo);
+  cardInfo.append(titleName);
+  cardInfo.append(username);
+  cardInfo.append(location);
+  cardInfo.append(profile);
+  cardInfo.append(followers);
+  cardInfo.append(following);
+  cardInfo.append(bio);
+  profile.append(aTagAddress);
+  
+  return mainCard;
+}
+
+
+
 
 /* List of LS Instructors Github username's: 
   tetondan
@@ -53,3 +119,25 @@ const followersArray = [];
   luishrd
   bigknell
 */
+let getLinks = followersArray.map(function(item){
+return `https://api.github.com/users/${item}`;
+});
+
+function cardCreator(arrayCard){
+    arrayCard.forEach(function(item){
+      axios.get(item)
+    .then( response => {
+        // Remember response is an object, response.data is an array.
+        console.log(response);
+        let UserInfo = UserCard(response);
+        let cards = document.querySelector('.cards');
+        cards.append(UserInfo);
+    })
+    .catch( error => {
+        console.log("Error:", error);
+    });
+    });
+}
+cardCreator(getLinks);
+
+
